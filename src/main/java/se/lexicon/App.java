8ae3dfe5f55d387da655a8ddccde19348891924c
Rasha9393 <rashadarwish93@gmail.com>
2024-05-14 13:43:40 +0200

    Implement all

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,76 @@
+package se.lexicon;
+
+import se.lexicon.data.PersonDao;
+import se.lexicon.data.impl.PersonDaoImpl;
+import se.lexicon.data.impl.TodoItemDaoImpl;
+import se.lexicon.model.Person;
+import se.lexicon.model.TodoItem;
+
+import java.awt.*;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       /* PersonDaoImpl personImpl =new PersonDaoImpl() ;
+        Person person1 = personImpl.findById(1);
+        System.out.println(person1);*/
+
+
+      /*  PersonDaoImpl personImpl =new PersonDaoImpl();
+        List<Person> person1 =personImpl.findAll();
+        person1.forEach(System.out::println);*/
+
+
+       /* PersonDaoImpl personIml =new PersonDaoImpl();
+        List<Person> person =personIml.findByName("");
+        person.forEach(System.out::println);*/
+
+
+       /* PersonDaoImpl personImpl =new PersonDaoImpl();
+        Person person =personIml.create(new Person(5000,"ahmad","aaaaaa"));
+        System.out.println("person insert ="+ person);*/
+
+
+
+       /* PersonDaoImpl personImpl =new PersonDaoImpl();
+        Person Ahmad=new Person(4000, "ahamad","aaaaaa");
+        Person person =personImpl.update(Ahmad);
+        System.out.println(person);*/
+
+
+       /* PersonDaoImpl person1 =new PersonDaoImpl();
+        boolean delete1 =PersonDaoImpl.deleteById(5000) ;
+        System.out.println(delete1);*/
+
+
+      /*  TodoItemDaoImpl todoImpl = new TodoItemDaoImpl();
+        TodoItem todo=todoImpl.findById(1);
+        System.out.println(todo);
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
